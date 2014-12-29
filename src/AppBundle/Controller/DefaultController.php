4ba1f81232370af00b789c6f9798ace63fe89343
Mushkin.V <mushkin.v@gmail.com>
2014-12-29 14:27:45 +0200

    adds translator options

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,24 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        if ($request->isMethod('POST')) {
+            $transLang = $request->request->get('transLang');
+            $text = $request->request->get('textArea');
+
+            return $this->render('default/index.html.twig',
+                ['transLang' => $transLang,
+                    'text' => $text,
+                ]);
+        }
+        return $this->render('default/index.html.twig',['transLang' => '1', 'text' => 'Enter text to translate']);
     }
 }
